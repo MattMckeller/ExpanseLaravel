4bc92e2a50c5fff033b5e204beddf8e8bd349fd3
Matt Mckeller <mattmckeller@gmail.com>
2017-10-05 17:28:33 -0500

    Create blank ecommerce repositories

---
+++ app/Repositories/Ecommerce/Checkout/CheckoutRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 10/5/17
+ * Time: 5:26 PM
+ */
+
+namespace App\Repositories\Ecommerce\Checkout;
+
+use Illuminate\Http\Request;
+
+class CheckoutRepository
+{
+
+}
