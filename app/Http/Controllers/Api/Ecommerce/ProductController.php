d8f2c800474465cce699f23d624ef3634d12d522
Matt Mckeller <mattmckeller@gmail.com>
2017-10-07 19:06:53 -0500

    Add get methods, Cleanup parameters

--- app/Http/Controllers/Api/Ecommerce/ProductController.php
+++ app/Http/Controllers/Api/Ecommerce/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Customers\Address;
 use App\Models\Customers\Customer;
 use App\Models\Customers\EmailAddress;
 use App\Models\Customers\PhoneNumber;
+use App\Models\Ecommerce\Product;
 use App\Models\Ecommerce\ProductImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -23,23 +24,36 @@ class ProductController extends ApiController
     private $rules = array(
     );
 
+
+    /**
+     * Get information about an individual product
+     * @param Product $product
+     */
+    function getProduct(Product $product){}
+
+    /**
+     * Get information about many products
+     * @param Request $request
+     */
+    function getProducts(Request $request){}
+
     /**
      * Add a new product to the database
      * @param Request $request
      */
-    function createNewProduct(Request $request){}
+    function createProduct(Request $request){}
 
     /**
      * Edit an existing product
-     * @param Request $request
+     * @param Product $product
      */
-    function editProduct(Request $request){}
+    function editProduct(Product $product){}
 
     /**
      * Soft delete an existing product
-     * @param Request $request
+     * @param Product $product
      */
-    function removeProduct(Request $request){}
+    function removeProduct(Product $product){}
 
     /**
      * Set an existing image as the primary image for a product.
