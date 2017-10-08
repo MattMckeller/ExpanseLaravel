7f8d3b86b8f90ce1820382b39a32c2f33db0f8db
Matt Mckeller <mattmckeller@gmail.com>
2017-10-07 19:16:53 -0500

    Create api routes for cart, checkout, payment types, products, product images, and shipping option controllers

--- routes/api.php
+++ routes/api.php
@@ -13,5 +13,45 @@ use Illuminate\Http\Request;
 |
 */
 
+//Inquiry Routes
 Route::post('/contactUs', 'Api\ContactController@submitContactForm');
 Route::post('/inquiry', 'Api\InquiryController@saveInquiry');
+
+//Checkout
+Route::post('/checkout', 'Api\Ecommerce\CartController@checkout');
+
+
+/** Restful Model Mapping Controllers **/
+//Cart
+Route::get('/cart/getProducts', 'Api\Ecommerce\CartController@getProducts');
+Route::post('/cart/addProduct', 'Api\Ecommerce\CartController@addProduct');
+Route::delete('/cart/removeProduct', 'Api\Ecommerce\CartController@removeProduct');
+Route::delete('/cart/emptyCart', 'Api\Ecommerce\CartController@emptyCart');
+
+//Payment types
+Route::get('/paymentTypes/getPaymentType/{PaymentType}', 'Api\Ecommerce\PaymentTypesController@getPaymentType');
+Route::get('/paymentTypes/getPaymentTypes', 'Api\Ecommerce\PaymentTypesController@getPaymentType');
+Route::post('/paymentTypes/addPaymentType', 'Api\Ecommerce\PaymentTypesController@addPaymentType');
+Route::post('/paymentTypes/editPaymentType', 'Api\Ecommerce\PaymentTypesController@editPaymentType');
+Route::delete('/paymentTypes/removePaymentType', 'Api\Ecommerce\PaymentTypesController@removePaymentType');
+
+//Products
+Route::get('/products/getProduct/{Product}', 'Api\Ecommerce\ProductsController@getProduct');
+Route::get('/products/getProducts', 'Api\Ecommerce\ProductsController@getProducts');
+Route::post('/products/createProduct', 'Api\Ecommerce\ProductsController@createProduct');
+Route::post('/products/editProduct/{Product}', 'Api\Ecommerce\ProductsController@editProduct');
+Route::delete('/products/removeProduct/{Product}', 'Api\Ecommerce\ProductsController@removeProduct');
+Route::post('/products/setPrimaryImage', 'Api\Ecommerce\ProductsController@setPrimaryImage');
+Route::post('/products/updateImageOrder', 'Api\Ecommerce\ProductsController@updateImageOrder');
+
+//Product Images
+Route::get('/productImages/addImage', 'Api\Ecommerce\ProductImageController@addImage');
+Route::delete('/productImages/removeImage/{ProductImage}', 'Api\Ecommerce\ProductImageController@removeImage');
+Route::post('/productImages/editImage', 'Api\Ecommerce\ProductImageController@editImage');
+
+//Shipping Options
+Route::get('/getOption/{ShippingOption}', 'Api\Ecommerce\ShippingOptionsController@getOption');
+Route::get('/getOptions', 'Api\Ecommerce\ShippingOptionsController@getOptions');
+Route::post('/addOption', 'Api\Ecommerce\ShippingOptionsController@addOption');
+Route::post('/editOption/{ShippingOption}', 'Api\Ecommerce\ShippingOptionsController@editOption');
+Route::delete('/removeOption', 'Api\Ecommerce\ShippingOptionsController@removeOption');
