e84cfe8b68f08f7b17fd7df2f3d2734e8399ed8b
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-10 14:32:09 -0500

    Stripe payment being charged. Commit is complete and total test code but it actually processes a charge.

--- app/Http/Controllers/Api/Ecommerce/CheckoutController.php
+++ app/Http/Controllers/Api/Ecommerce/CheckoutController.php
@@ -22,5 +22,55 @@ class CheckoutController extends Controller
 
     public function checkout(Request $request) {
         pretty_print_r($request->all());
+        $inputData = $request->all();
+        \Stripe\Stripe::setApiKey(config('stripe.apiKey'));
+        $cost = $this->calculateTotal($inputData['products'], $inputData['shippingSelection']);
+        $stripeAmount = $this->convertToStripeAmount($cost);
+        $customer = $this->saveCustomer($inputData);
+
+        $charge = \Stripe\Charge::create(array(
+            "amount" => $stripeAmount,
+            "currency" => "usd",
+            "description"=> "VintageHails purchase.",
+            "source" => $request->input('stripeToken')
+        ));
+
+        $successful = ($charge['status'] == 'succeeded')?(true):(false);
+        if($successful == true){
+            return Response(array(
+                'success'=>$successful
+            ));
+        }else{
+            $error = ($charge['status'] == 'error')?(true):(false);
+            $errorMessage = ($error==true) ?
+                ($this->getStripeErrorMessage($charge['status'])) : ('');
+            return Response(
+                array(
+                    'error'=>$error,
+                    'errorMessage'=>$errorMessage
+                )
+            );
+        }
+    }
+
+    function calculateTotal($products, $shippingSelection){
+        $total = 0.00;
+        foreach($products as $product){
+            $total += $product['price'];
+        }
+        $total += $shippingSelection['cost'];
+        return $total;
+    }
+
+    function saveCustomer($data){
+
+    }
+
+    function convertToStripeAmount($usd){
+        return $usd*100;
+    }
+
+    function getStripeErrorMessage($status){
+        return 'An error occurred, status was: ' . $status;
     }
 }
