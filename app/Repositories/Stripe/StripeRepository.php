33a88bf6404d30feacc1975f75fec4536b6ee735
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-10 14:30:25 -0500

    Add stripe repository and stripe api key environment config

---
+++ app/Repositories/Stripe/StripeRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Repositories\Stripe;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class StripeRepository
+{
+    function charge($token, $amount){
+
+    }
+}
