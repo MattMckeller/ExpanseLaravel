5b52fd676f7b05c9c3b88641153af6bf1c0077da
Matt Mckeller <mattmckeller@gmail.com>
2017-10-13 16:45:50 -0500

    Add throttling and binding to vintagehails routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,11 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+
+        'vintageHails'=>[
+            'throttle: 60,1',
+            'bindings'
+        ]
     ];
 
     /**
